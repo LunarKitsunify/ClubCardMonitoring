39c218c6720876bc4e623d5e122c8a1b9872d0e1
LunarKitsunify <kitsunify@email.com>
2025-06-25 17:06:34 +0600

    !

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import db_info
+from .views import db_info, home
 
 urlpatterns = [
+    path('', home),
     path('db-check/', db_info),
 ]
--- monitoring/views.py
+++ monitoring/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from django.db import connection
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to ClubCard Monitoring!")
 
 def db_info(request):
     with connection.cursor() as cursor:
