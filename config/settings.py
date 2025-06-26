0239a5487899863e7682a0baa8a3ff1c460eec34
LunarKitsunify <kitsunify@gmail.com>
2025-06-26 09:11:45 +0600

    !

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,6 @@ import dj_database_url
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
