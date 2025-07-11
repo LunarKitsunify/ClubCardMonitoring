64f50a85b2af92b1eadb81148cc0d60e67f87388
LunarKitsunify <kitsunify@gmail.com>
2025-07-12 03:25:19 +0600

    !

--- monitoring/utils/processing_meta.py
+++ monitoring/utils/processing_meta.py
@@ -5,8 +5,8 @@ from django.utils.timezone import now
 from django.conf import settings
 
 PROCESSING_FILE = os.path.join(settings.BASE_DIR, "static", "processing_meta.json")
-# PROCESS_INTERVAL = timedelta(hours=3)
-PROCESS_INTERVAL = timedelta(minutes=5)
+PROCESS_INTERVAL = timedelta(hours=3)
+# PROCESS_INTERVAL = timedelta(minutes=5)
 
 def read_next_processing_time():
     if not os.path.exists(PROCESSING_FILE):
